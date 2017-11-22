743277f75491feee7600c9cefddbd1b220ebfaca
Frank Xu <frank.xu@northbright.com>
2017-11-22 11:22:20 +0800

    Replace '1000' string with 1000 int for ZSCAN command.

--- tools/get-phones-which-have-2-or-more-students/main.go
+++ tools/get-phones-which-have-2-or-more-students/main.go
@@ -67,7 +67,7 @@ func FindPhones(redisServer, redisPassword string) error {
 	k := "phones"
 	cursor := 0
 	for {
-		if v, err = redis.Values(conn.Do("ZSCAN", k, cursor, "COUNT", "1000")); err != nil {
+		if v, err = redis.Values(conn.Do("ZSCAN", k, cursor, "COUNT", 1000)); err != nil {
 			return err
 		}
 
