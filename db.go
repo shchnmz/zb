f51b0629590a6d48d417454e01836d5f3c489f84
Frank Xu <frank.xu@northbright.com>
2019-11-06 10:27:05 +0800

    Update redigo/redis package address
    * github.com/garyburd/redigo/redis -> github.com/gomodule/redigo/redis

--- db.go
+++ db.go
@@ -9,7 +9,7 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/garyburd/redigo/redis"
+	"github.com/gomodule/redigo/redis"
 	"github.com/northbright/pathhelper"
 	"github.com/northbright/redishelper"
 	"github.com/shchnmz/ming"
